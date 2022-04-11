ff2057084608bc5e55990edfa20da2da1e16a6f8
Anupam-dagar <dagaranupam@gmail.com>
2022-04-11 23:10:18 +0530

    implement metamask connection.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,61 @@
 import "./styles/App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Constants
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
-  // Render Methods
+  const [currentAccount, setCurrentAccount] = useState("");
+
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
       Connect to Wallet
     </button>
   );
+
+  const isWalletConnected = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("metamask not installed.");
+      return;
+    }
+    console.log("metamask is installed", ethereum);
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found accounts: ", account);
+      setCurrentAccount(account);
+      return;
+    }
+
+    console.log("no metamask account found.");
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Please install metamask to continue.");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected accounts:", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    isWalletConnected();
+  }, []);
 
   return (
     <div className="App">
@@ -21,7 +65,13 @@ const App = () => {
           <p className="sub-text">
             Discover your unique mathematically generated NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button onClick={null} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
         </div>
       </div>
     </div>
